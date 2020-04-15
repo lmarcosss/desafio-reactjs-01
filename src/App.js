64bc45a4abd2fcaff466ef4c641de61c6ea86e87
Leonardo Marcos <leonardo.marcos@cwi.com.br>
2020-04-15 20:00:25 -0300

    fix: modify app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { repositoriesService } from './services'
-import { Form } from './components'
 import "./styles.css";
 
 function App() {
   const [repositories, setRepositories] = useState([])
-  const [title, setTitle] = useState('')
-  const [url, setUrl] = useState('')
-  const [techs, setTechs] = useState('')
 
 
   useEffect(() => {
@@ -16,18 +12,13 @@ function App() {
 
   async function handleAddRepository() {
     try {
-      const hasEmptyInputs = [title, url, techs].some((value) => value.length === 0)
-
-      if (!hasEmptyInputs) {
-        const newTechs = techs.split(',')
         const repository = await repositoriesService.createRepository({
-          title,
-          url,
-          techs: newTechs,
+          title: `Novo projeto ${Date.now()}`,
+          url: 'https://github.com/lmarcosss/desafio-reactjs-01',
+          techs: ['ReactJS', 'React Native', 'NodeJS'],
         })
 
         setRepositories((previousRepositories) => [...previousRepositories, repository])
-      }
 
     } catch {
       alert('Problema ao criar repositório.')
@@ -44,55 +35,26 @@ function App() {
     }
   }
 
-  const inputs = [
-    {
-      id: "title",
-      label: "Título",
-      onChange: setTitle,
-      value: title,
-    },
-    {
-      id: "url",
-      label: "Página",
-      onChange: setUrl,
-      value: url,
-    },
-    {
-      id: "techs",
-      label: "Tecnologia",
-      onChange: setTechs,
-      value: techs,
-    }
-  ]
-
   return (
-    <div className="repository-container">
-      <header>
-        <h1 className="text">Repositórios do Github</h1>
-      </header>
-      <div className="content">
-        <Form
-          onClick={handleAddRepository}
-          inputs={inputs}
-          textButton="Adicionar"
-        />
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(({ id, url, title, techs }) => (
+          <li key={id}>
+            <p>{title}</p>
+            <p>{url}</p>
+            <div className="container-techs">
+              {techs.map((tech) => (
+                <p key={tech}>{tech}</p>
+              ))}
+            </div>
+            <button onClick={() => handleRemoveRepository(id)}>
+              Remover
+            </button>
+          </li>
+        ))}
+      </ul>
 
-        <ul className="repository-list" data-testid="repository-list">
-          {repositories.map(({ id, url, title, techs }) => (
-            <li key={id}>
-              <a href={url} target="_blank" rel="noopener noreferrer">{title}</a>
-              <div className="container-techs">
-                {techs.map((tech) => (
-                  <p key={tech}>{tech}</p>
-                ))}
-              </div>
-              <button onClick={() => handleRemoveRepository(id)}>
-                Remover
-             </button>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
